00a7a86608394b2c5d8ef15ddfa31bd3bcde4612
Chaitali Rane <ranec9753@gmail.com>
2026-04-24 20:05:50 +0530

    added theme & made changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Dashboard from "./pages/Dashboard";
+import SubmitNeed from "./pages/SubmitNeed";
+import RegisterVolunteer from "./pages/RegisterVolunteer";
+import VolunteerMatch from "./pages/VolunteerMatch";
 
-function App() {
+export default function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
-        <Route path="/" element={<div>Home - coming soon</div>} />
+        <Route path="/" element={<Navigate to="/dashboard" replace />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/submit" element={<SubmitNeed />} />
+        <Route path="/register" element={<RegisterVolunteer />} />
+        <Route path="/match" element={<VolunteerMatch />} />
       </Routes>
     </BrowserRouter>
   );
 }
-
-export default App;
